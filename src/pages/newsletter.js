56579e51194d86bf8a3418562d58a01d0b6e40e9
shina <rasheedshinaopeyemi@gmail.com>
2020-07-11 21:49:50 +0100

    added netlify form

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -7,7 +7,14 @@ const NewsLetter = () => {
         <div className="page-center">
           <h2>Get all the latest stories to your inbox</h2>
           <h4>I write to my friends every few weeks</h4>
-          <form className="contact-form">
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            className="contact-form"
+            netlify-honeypot="bot-field"
+            action="/success"
+          >
             <input
               type="text"
               name="name"
